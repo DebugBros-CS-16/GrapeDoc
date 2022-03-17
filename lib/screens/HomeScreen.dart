8061a662917eadbfce5c18b903aa2367fd369d2a
ShaleenW <eranda.2019737@iit.ac.lk>
2022-03-17 13:44:42 +0530

    Modified Image processing screen interface.

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -19,77 +19,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return
-       HomeNews();
-    //   Container(
-    //   height: double.infinity,
-    //   width: double.infinity,
-    //   child: ListView(
-    //     scrollDirection: Axis.vertical,
-    //     children: [
-    //       Container(
-    //         margin: const EdgeInsets.all(10.0),
-    //         height: 250.0,
-    //         child: ListView(
-    //           scrollDirection: Axis.horizontal,
-    //           children: [
-    //             Container(
-    //               margin: const EdgeInsets.only(right: 10.0),
-    //               width: 200.0,
-    //               color: Colors.purple,
-    //             ),
-    //             Container(
-    //               margin: const EdgeInsets.only(right: 10.0),
-    //               width: 200.0,
-    //               color: Colors.green,
-    //             ),
-    //             Container(
-    //               margin: const EdgeInsets.only(right: 10.0),
-    //               width: 200.0,
-    //               color: Colors.pink,
-    //             ),
-    //             Container(
-    //               margin: const EdgeInsets.only(right: 10.0),
-    //               width: 200.0,
-    //               color: Colors.purple,
-    //             ),
-    //             Container(
-    //               margin: const EdgeInsets.only(right: 10.0),
-    //               width: 200.0,
-    //               color: Colors.green,
-    //             ),
-    //             Container(
-    //               width: 200.0,
-    //               color: Colors.pink,
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //       Container(
-    //         height: 200.0,
-    //         width: double.infinity,
-    //         margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-    //         color: Colors.blue,
-    //       ),
-    //       Container(
-    //         height: 200.0,
-    //         width: double.infinity,
-    //         margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-    //         color: Colors.red,
-    //       ),
-    //       Container(
-    //         height: 200.0,
-    //         width: double.infinity,
-    //         margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-    //         color: Colors.green,
-    //       ),
-    //       Container(
-    //         height: 200.0,
-    //         width: double.infinity,
-    //         margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-    //         color: Colors.amber,
-    //       ),
-    //     ],
-    //   ),
-    // );
+    //   HomeNews();
+      Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            height: 250.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 10.0),
+                  width: 200.0,
+                  color: Colors.purple,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 10.0),
+                  width: 200.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 10.0),
+                  width: 200.0,
+                  color: Colors.pink,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 10.0),
+                  width: 200.0,
+                  color: Colors.purple,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 10.0),
+                  width: 200.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 200.0,
+                  color: Colors.pink,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            color: Colors.blue,
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            color: Colors.red,
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            color: Colors.green,
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            color: Colors.amber,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/testImagePicker.dart
+++ lib/screens/testImagePicker.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -43,6 +44,10 @@ class _TestImagePickerState extends State<TestImagePicker> {
 
   Category? category;
 
+  bool _isLoading = false;
+
+  final isDialOpen = ValueNotifier(false);
+
   @override
   void initState() {
     super.initState();
@@ -56,7 +61,7 @@ class _TestImagePickerState extends State<TestImagePicker> {
       _image = File(pickedFile!.path);
       _imageWidget = Image.file(_image!);
 
-      _predict();
+      imageCheck();
     });
   }
 
@@ -67,8 +72,18 @@ class _TestImagePickerState extends State<TestImagePicker> {
       _image = File(pickedFile!.path);
       _imageWidget = Image.file(_image!);
 
-      _predict();
+      imageCheck();
     });
+  }
+
+  Future imageCheck() async{
+    if(_image != null){
+
+        setState(() {
+          _isLoading = true;
+        });
+      }
+    _predict();
   }
 
   void _predict() async {
@@ -82,56 +97,107 @@ class _TestImagePickerState extends State<TestImagePicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Center(
-            child: _image == null
-                ? Text('No image selected.')
-                : Container(
-                    constraints: BoxConstraints(
-                        maxHeight: MediaQuery.of(context).size.height / 2),
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                    ),
-                    child: _imageWidget,
-                  ),
+    return WillPopScope(
+      onWillPop: () async {
+        if (isDialOpen.value) {
+          isDialOpen.value = false;
+          return false;
+        }else{
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              Container(
+                child: _image == null
+                    ? Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                        height: 300.0,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.black12,
+                            borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(child: const Text('Select Image File'))
+                )
+                    : Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                        height: 300.0,
+                        width: MediaQuery.of(context).size.width,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: _isLoading ? Image.file(
+                               _image!,
+                              fit: BoxFit.cover,
+                            ): Center(child: CircularProgressIndicator()),
+                            ),
+                      ),
+              ),
+              SizedBox(
+                height: 36,
+              ),
+              Text(
+                category != null ? category!.label : '',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                category != null
+                    ? 'Confidence: ${category!.score.toStringAsFixed(3)}'
+                    : '',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 36,
-          ),
-          Text(
-            category != null ? category!.label : '',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Text(
-            category != null
-                ? 'Confidence: ${category!.score.toStringAsFixed(3)}'
-                : '',
-            style: TextStyle(fontSize: 16),
-          ),
-        ],
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: getImageFromCamera,
-            tooltip: 'Pick Image From Camera',
-            child: Icon(Icons.add_a_photo),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          FloatingActionButton(
-            onPressed: getImageFromGallery,
-            tooltip: 'Pick Image From Gallery',
-            child: Icon(Icons.add_photo_alternate_outlined),
-          ),
-        ],
+        ),
+        floatingActionButton: SpeedDial(
+          icon: Icons.add,
+          animatedIcon: AnimatedIcons.menu_close,
+          backgroundColor: Colors.purple,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.4,
+          spacing: 10,
+          spaceBetweenChildren: 8,
+          //closeManually: true,
+          openCloseDial: isDialOpen,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.add_photo_alternate_outlined),
+              backgroundColor: Colors.purple,
+              label: 'Device',
+              onTap: getImageFromGallery
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.add_a_photo),
+              backgroundColor: Colors.purple,
+              label: 'Camera',
+              onTap: getImageFromCamera
+            )
+          ],
+        )
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.end,
+        //   children: [
+        //     FloatingActionButton(
+        //       onPressed: getImageFromCamera,
+        //       tooltip: 'Pick Image From Camera',
+        //       child: Icon(Icons.add_a_photo),
+        //     ),
+        //     const SizedBox(
+        //       height: 10.0,
+        //     ),
+        //     FloatingActionButton(
+        //       onPressed: getImageFromGallery,
+        //       tooltip: 'Pick Image From Gallery',
+        //       child: Icon(Icons.add_photo_alternate_outlined),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
